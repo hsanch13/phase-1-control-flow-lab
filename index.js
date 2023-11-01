28931dfc0ee73010211f5a779cffb4df7872e37a
hsanch13 <hayleysanchez13@gmail.com>
2023-11-01 11:56:40 -0600

    added conditional statements

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber <= 400) {
+  return "This one is on me!";
+
+}  else if (someNumber >= 401 && someNumber <= 2000){
+  return "That will be twenty bucks.";
+}  
+  else if (someNumber >= 2000 && someNumber <= 2500){
+  return "I will gladly take your thirty bucks.";
+  }
+
+  else if (someNumber > 2500){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(Destination){
+  return Destination === "NYC" ? "Ok, sounds good." : "No go."
 }
+ternaryCheckCity("NYC")
+ternaryCheckCity("Pittsburg")
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(Amount){
+  switch (Amount) {
+    case "generous":
+    return "Thank you so much."
+
+    case "not as generous":
+    return "Thank you."
+
+    default:
+      return "Bye." 
+  }
+
 }
